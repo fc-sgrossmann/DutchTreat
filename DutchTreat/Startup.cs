d1e110f836139cc5675444efa337d70f47056ab4
Stefan Grossmann <stefan.grossmann68@gmail.com>
2017-11-24 12:43:10 +0100

    Lektion 1

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -20,15 +20,17 @@ namespace DutchTreat
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+			//if (env.IsDevelopment())
+			//{
+			//    app.UseDeveloperExceptionPage();
+			//}
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+			//app.Run(async (context) =>
+			//{
+			//    await context.Response.WriteAsync("<h1>Hello Stefan!</h1>");
+			//});
+
+			app.UseStaticFiles();
         }
     }
 }
