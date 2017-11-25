9aca1d1ac26c6848ac60e91a3316bc63512190a7
Stefan Grossmann <stefan.grossmann68@gmail.com>
2017-11-25 12:00:17 +0100

    added jquery & bootstrap

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -2,3 +2,14 @@
 
 var theForm = document.getElementById("theForm");
 theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+	console.log("Gekauft");
+});
+
+
+var productInfo = document.getElementsByClassName("product-props");
+var listItems = productInfo.item[0].children;
+
+
