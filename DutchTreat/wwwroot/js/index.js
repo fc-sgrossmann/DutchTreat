1f11951d3102da733235b6a77af1b54ada67d333
Stefan Grossmann <stefan.grossmann68@gmail.com>
2017-11-25 14:10:54 +0100

    javascript mit JQuery

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,17 +1,30 @@
-﻿console.log("Hallo Stefan");
+﻿$(document).ready(function () {
 
-var theForm = $("#theForm");
-theForm.hide();
 
-var button = $("#buyButton");
-button.on("click", function () {
-	console.log("Gekauft");
+	console.log("Hallo Stefan");
+
+	var theForm = $("#theForm");
+	theForm.hide();
+
+	var button = $("#buyButton");
+	button.on("click", function () {
+		console.log("Gekauft");
+	});
+
+
+	var productInfo = $(".product-props li");
+	productInfo.on("click", function () {
+		console.log("Sie haben auf " + $(this).text());
+	});
+
+	var $loginToggle = $("#loginToggle");
+	var $popupForm = $(".popup-form");
+
+	$loginToggle.on("click", function () {
+		$popupForm.fadeToggle(1000);
+	});
+
+
+
 });
-
-
-var productInfo = $(".product-props li");
-productInfo.on("click", function () {
-	console.log("Sie haben auf " + $(this).text());
-});
-
 
